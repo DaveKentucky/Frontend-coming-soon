501740de97277b5329c97bed930c89a1c6cfc35d
DaveKentucky <didam7@wp.pl>
2021-09-20 12:49:02 +0200

    Improved styling of the form elements

--- email-verification.js
+++ email-verification.js
@@ -1,9 +1,20 @@
-import variables from './variables.scss';
-
 const emailForm = document.getElementById('email-form');
+const emailTextbox = document.getElementById('email-address');
+const emailButton = document.getElementById('email-button');
+const form = document.querySelector('form');
+const errorElements = document.querySelectorAll('.error');
+
+
+emailTextbox.addEventListener('click', () => {
+    errorElements.forEach(element => {
+        if(element.id === 'error-icon') {
+            element.style.display = 'none';
+        }
+    });
+})
 
 function validateForm() {
-    const email = document.getElementById('email-address').value;
+    const email = emailTextbox.value;    
     valid = true;
     if (email === "") {
         valid = false;
@@ -11,7 +22,20 @@ function validateForm() {
         valid = validateEmail(email);
     }
     if (!valid) {
-        document.querySelector('form').style.borderColor = variables.errorColor;
+        form.style.borderColor = 'hsl(0, 93%, 68%)';
+        form.style.borderWidth = '2px';
+        emailButton.style.margin = '-2px';
+        errorElements.forEach(element => {
+            element.style.display = 'block';{
+            element.animate([
+                { transform: 'translateY(0px)' },
+                { transform: 'translateY(-10px)' },
+                { transform: 'translateY(0px)'},
+                { transform: 'translateY(-5px)' },
+                { transform: 'translateY(0px)' },
+            ], {duration: 500})
+            }
+        });
     }
     return valid;
 }
